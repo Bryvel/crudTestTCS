4b7addba15adde2f2a72f5d8f4a0e6c58027bc37
Bryan Velasco <bryvel_1101@yahoo.com>
2026-01-29 02:26:14 -0500

    update services

--- src/test/java/com/crud/test/TestApplicationTests.java
+++ src/test/java/com/crud/test/TestApplicationTests.java
@@ -3,11 +3,20 @@ package com.crud.test;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.crud.test.domain.Client;
+
 @SpringBootTest
 class TestApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+     void crearCliente_ok() {
+    Client cliente = new Client();
+     cliente.setNombre("Jose Lema");
+
+}
 
 }
